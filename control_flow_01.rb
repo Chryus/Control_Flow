e57d580778f75133ca1cca2fea42d593bcbd9f2e
Chryus <chris.haack@me.com>
2013-10-22 22:57:44 -0400

    add break keyword to for loop

--- control_flow_01.rb
+++ control_flow_01.rb
@@ -2,3 +2,8 @@ for animal in %w(dog cat horse goat rabbit mouse mole) do
 	next if animal == "cat"
 	puts "The current animal is #{animal}"
 end
+
+for animal in %w(dog cat horse goat rabbit mouse mole) do
+	break if animal == "cat"
+	puts "The current animal is #{animal}"
+end
