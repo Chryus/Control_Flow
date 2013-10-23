1dc5f21c81f9a3b5cc13cb91f2a537dc891f8250
Chryus <chris.haack@me.com>
2013-10-22 23:26:23 -0400

    add loop iterator with break and downcase method

--- control_flow_01.rb
+++ control_flow_01.rb
@@ -7,3 +7,15 @@ for animal in %w(dog cat horse goat rabbit mouse mole) do
 	break if animal == "cat"
 	puts "The current animal is #{animal}"
 end
+
+name = ""
+
+loop do
+	print "Enter your name: "
+	name = gets.chomp
+	if name == "Jason"
+		name.downcase
+		puts "Hi #{name}", we've been expecting you."
+		break
+	end
+end
